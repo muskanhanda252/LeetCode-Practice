dc4a8ebd6626d91ab21fd91f50893591a23210a9
Muskan Handa <500068628@stu.upes.ac.in>
2022-03-25 14:37:58 +0530

    Time: 51 ms (52.89%), Space: 27.5 MB (63.85%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0;
+        int n=nums.size();
+        int end=n-1;
+        while(start<=end)
+        {
+            int mid=((start+end)/2);
+            if(target==nums[mid])
+                return mid;
+            else if(target>nums[mid])
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        return -1;
+    }
+};
